893fbc73249946a568dd619b282a7ee035db5466
roxmysox <harrison@mitgangs.com>
2016-10-24 21:33:47 -0700

    fixed mobile issue and added avg

--- script.js
+++ script.js
@@ -2,11 +2,25 @@ var lastTapSeconds = 0;
 var bpm = 0;
 
 var tapDiv = document.getElementById("tapSpan");
+var avg = document.getElementById("avg");
 
-document.addEventListener('click', function() {
-    var tapSeconds = new Date().getTime();
+var hist = [];
+
+function add(a, b) {
+    return a + b;
+}
+
+var tap = function () {
+	var tapSeconds = new Date().getTime();
 
     bpm = ((1 / ((tapSeconds - lastTapSeconds) / 1000)) * 60);
     lastTapSeconds = tapSeconds;
-    tapSpan.innerHTML = Math.round(bpm * 10) / 10;
-});
+    var out = Math.round(bpm * 10) / 10;
+    tapSpan.innerHTML = out;
+    console.log(hist);
+    hist.push(out);
+    avg.innerHTML = hist.reduce(add, 0)/hist.length;
+};
+
+document.addEventListener('click', tap);
+document.addEventListener('touchstart', tap);
